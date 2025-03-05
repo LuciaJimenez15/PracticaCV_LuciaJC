44163e7de0656e730a147188973bcaea25db306a
Equipo <Equipo@DESKTOP-9FPT941>
2025-03-05 18:47:08 +0100

    Agregar archivos de proyecto.

---
+++ PracticaCV_LuciaJC/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaCV_LuciaJC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
